2df85e2112aaea77ccdf4320c76d0cef90311f25
Alex <agasparr98@gmail.com>
2020-12-30 14:58:06 +0100

    exo fini

---
+++ public/js/main.js
@@ -0,0 +1,20 @@
+// - # Exercice 3 JavaScript -> Array
+
+//     - ## Créer une variable qui reçois un array avec 3 éléments -> ex: pomme, cerise, fraise
+
+let fruits = ["pomme","cerise","fraise"];
+
+//     - ## Afficher les éléments de l'array
+
+console.log(fruits);
+
+//     - ## Ajouter votre prenom à l'array et afficher son contenu
+
+fruits.push("Alex");
+console.log(fruits);
+
+//     - ## Remplacer et afficher un élément de l'array par autre chose
+
+let i = 1;
+fruits[i] = "poopy";
+console.log(fruits);
